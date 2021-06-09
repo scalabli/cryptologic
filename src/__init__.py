47a6d2a816c7206832fce81592b51a8f07e170bb
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-09 22:07:37 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,11 +2,30 @@
 Python wrapper for cryptocompare API
 """
 
+import requests
+import time
+import quo
+import datetime
+import typing
+import os
+from typing import Union, Optional, List, Dict
+Timestamp = Union[datetime.datetime, datetime.date, int, float]
 
+# API
+_API_KEY_PARAMETER = ""
+_URL_COIN_LIST = 'https://www.cryptocompare.com/api/data/coinlist?'
+_URL_PRICE = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms={}&tsyms={}'
+_URL_PRICE_MULTI = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms={}&tsyms={}'
+_URL_PRICE_MULTI_FULL = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms={}'
+_URL_HIST_PRICE = 'https://min-api.cryptocompare.com/data/pricehistorical?fsym={}&tsyms={}&ts={}&e={}'
+_URL_HIST_PRICE_DAY = 'https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym={}&limit={}&e={}&toTs={}'
+_URL_HIST_PRICE_HOUR = 'https://min-api.cryptocompare.com/data/histohour?fsym={}&tsym={}&limit={}&e={}&toTs={}'
+_URL_HIST_PRICE_MINUTE = 'https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit={}&e={}&toTs={}'
+_URL_AVG = 'https://min-api.cryptocompare.com/data/generateAvg?fsym={}&tsym={}&e={}'
+_URL_EXCHANGES = 'https://www.cryptocompare.com/api/data/exchanges?'
+_URL_PAIRS = 'https://min-api.cryptocompare.com/data/pair/mapping/exchange?e={}'
 
-
-
-from .cryptologic import *
-
-
-__version__ = "1.0.1"
+# DEFAULTS
+CURRENCY = 'USD'
+LIMIT = 1440
+###############################################################################
